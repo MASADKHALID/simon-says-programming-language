e4016490abe3cbc812b5ec4aa4e75dee0d722440
Muhammad Asad Khalid <asadkhalidking55@gmail.com>
2025-04-23 01:30:22 +0500

    update 4/23/25

--- Lexicalanalyzer.py
+++ Lexicalanalyzer.py
@@ -4,9 +4,9 @@ import re
 token_specification = [
     ('KEYWORD',   r'\bsimon\b|\bsays\b|\bprint\b|\bint\b|\bfloat\b|\bchar\b|\bstring\b|\bbool\b|\bset\b|\bif\b|\belse\b|\belif\b|\bfor\b|\bwhile\b'),  # Keywords like int, float, char
     ('STRING',    r'"[^"]*"'),
-    ('IDENTIFIER', r'[A-Za-z_]\w*'),             # Identifiers
+    ('IDENTIFIER', r'[A-Za-z_]\w*'),              # Identifiers
     ('NUMBER',    r'\d+'),                       # Integer numbers
-    ('OPERATOR',  r'[=+\-*/]'),                  # Operators
+    ('OPERATOR', r'==|!=|>=|<=|&&|\|\||\+\+|--|\+=|-=|\*=|/=|=|>|<|\+|-|\*|/'),                  # Operators
     ('PUNCTUATION', r'[;\[\]\{\},\(\)\'\"]'),    # Punctuation
     ('SKIP',      r'[ \t]+'),                    # Skip spaces and tabs
     ('MISMATCH',  r'.'),                         # Any other character
@@ -17,21 +17,19 @@ def symboltable():
     for i in symbol_table:
         print(f"{i}\n")
 def lexer(code,lineno):
-    for i in code:
-        tok_regex = '|'.join(f'(?P<{name}>{pattern})' for name, pattern in token_specification)
-    
+    tok_regex = '|'.join(f'(?P<{name}>{pattern})' for name, pattern in token_specification)
+    #print(tok_regex)
     # Use re.finditer() to match all patterns in the string
     for mo in re.finditer(tok_regex, code):
+        #print(code)
+        #print(mo)
         class_part = mo.lastgroup
         value_part = mo.group()
-        
-        # Skip spaces (SKIP token)
         if class_part == 'SKIP':
             continue
-        '''
-        elif kind == 'MISMATCH':
-            raise RuntimeError(f'Unexpected character {value!r}')
-        '''
+        # mismatch hae some some error so use alid words for checking
+        elif class_part == 'MISMATCH': 
+            raise RuntimeError(f'Unexpected character {value_part!r}')
         symbol_table.append(str(lineno)+" : "+class_part+" : "+value_part)
     return symbol_table
 
